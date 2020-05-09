b0563b6751520427c396075a8329d7b90d1890da
Emmanuel Okwara <emma4real37@gmail.com>
2020-05-09 05:39:26 +0100

    IBOutlet and IBAction

--- Events/ViewController.swift
+++ Events/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toggleSwitch: UISwitch!
+    @IBOutlet weak var headerLbl: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitBtnClicked(_ sender: UIButton) {
+        let name = nameField.text ?? ""
+        headerLbl.text = "Welcome, \(name)"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameField.resignFirstResponder()
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            self.view.backgroundColor = .white
+        } else {
+            self.view.backgroundColor = .darkGray
+        }
+    }
+    
 }
 
