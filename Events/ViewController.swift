cee3f81515d48fc04932b046f7d5b2d7a8d74fbd
Emmanuel Okwara <emma4real37@gmail.com>
2020-05-09 03:36:20 +0100

    Initial Commit

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Emmanuel Okwara on 09/05/2020.
+//  Copyright © 2020 Emmanuel Okwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
