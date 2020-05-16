e350b54cf79804e3bee3e63417b5d26e7f16e09c
Emmanuel Okwara <emma4real37@gmail.com>
2020-05-16 02:55:16 +0100

    Initial Commit

---
+++ Animal Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animal Gallery
+//
+//  Created by Emmanuel Okwara on 16/05/2020.
+//  Copyright © 2020 Emmanuel Okwara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
