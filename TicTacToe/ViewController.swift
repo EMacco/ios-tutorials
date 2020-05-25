5a4b6d54b8ef1d3ae92b481f373ddbf422ebecc3
Emmanuel Okwara <emma4real37@gmail.com>
2020-05-25 17:04:36 +0100

    TicTacToe Starter

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -3,18 +3,34 @@
 //  TicTacToe
 //
 //  Created by Emmanuel Okwara on 24/05/2020.
-//  Copyright © 2020 Skarpa. All rights reserved.
+//  Copyright © 2020 Macco. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var startBtn: UIButton!
+    @IBOutlet weak var cardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
 
-
+    func setupUI() {
+        startBtn.layer.cornerRadius = 10
+        cardView.layer.cornerRadius = 10
+        cardView.layer.shadowOpacity = 0.4
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowRadius = 10
+        cardView.layer.shadowOffset = .zero
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        nameField.resignFirstResponder()
+    }
 }
 
