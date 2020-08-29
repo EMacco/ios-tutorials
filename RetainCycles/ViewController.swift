b346ee19ca004100f15a6392773cd1c2a4aa0549
Emmanuel Okwara <emma4real37@gmail.com>
2020-08-29 08:36:07 +0100

    Retain Cycle

--- RetainCycles/ViewController.swift
+++ RetainCycles/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var peter: User?
+    var louis: User?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        peter = User(firstName: "Peter", lastName: "Griffin")
+        louis = User(firstName: "Louis", lastName: "Quagmire")
+        
+        peter?.spouse = louis // 1
+        louis?.spouse = peter // 1
+        
+//        peter = nil // 0
+//        louis = nil
+        
+        navigationController?.pushViewController(SecondViewController(), animated: true)
     }
 
 
 }
 
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .cyan
+        
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name("macco"), object: nil, queue: .main) { [weak self] (_) in
+            self?.view.backgroundColor = .red
+        }
+    }
+    
+    deinit {
+        print("Second VC has been removed from memory")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+class User {
+    let firstName: String
+    let lastName: String
+    weak var spouse: User?
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    deinit {
+        print("\(firstName) has been removed from memory")
+    }
+}
