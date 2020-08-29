9f79c88f6df7198ade3ef8c3748c466bbbdc19a1
Emmanuel Okwara <emma4real37@gmail.com>
2020-08-29 08:39:15 +0100

    Semaphores

--- Semaphores/ViewController.swift
+++ Semaphores/ViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var movies: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let semaphore = DispatchSemaphore(value: 1)
+        let queue = DispatchQueue.global()
+        
+        queue.async {
+            print("I am in the first block before wait")
+            semaphore.wait() // 0
+            print("I am in the first block after wait")
+            self.movies.append(self.downloadMovie("Avengers"))
+            semaphore.signal()
+        }
+        
+        queue.async {
+            print("I am in the second block before wait")
+            semaphore.wait()
+            print("I am in the second block after wait")
+            self.saveMovies()
+            self.movies.remove(at: 0)
+            semaphore.signal()
+        }
+        
+        
+        
+        
+        
+//        let group = DispatchGroup()
+//        let queue = DispatchQueue.global()
+//
+//        queue.async(group: group) {
+//            let movieName = self.downloadMovie("Avengers")
+//            self.movies.append(movieName)
+//        }
+//
+//        queue.async(group: group) {
+//            self.saveMovies()
+//            self.movies.remove(at: 0)
+//        }
+//
+//        group.notify(queue: .main) {
+//            print("All downloads have been completed")
+//        }
+        
+        print("I ain't waiting for no computer")
     }
 
-
+    func downloadMovie(_ name: String) -> String {
+        sleep(4)
+        print("\(name) has been downloaded")
+        return name
+    }
+    
+    func saveMovies() {
+        sleep(2)
+        print("Movies have been saved")
+    }
 }
 
