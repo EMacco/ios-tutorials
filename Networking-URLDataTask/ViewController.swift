94de06e9f051cc5079428c01ed7f4d599041920c
Emmanuel Okwara <emma4real37@gmail.com>
2020-12-06 14:53:59 +0100

    urlsession dataTask

--- Networking-URLDataTask/ViewController.swift
+++ Networking-URLDataTask/ViewController.swift
@@ -11,9 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let params = [
+            "title": "Macco is the best",
+            "body": "This is the body of the post. Get over it faaaaam"
+        ]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        
+        let session = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("The error was: \(error.localizedDescription)")
+            } else {
+                let jsonRes = try? JSONSerialization.jsonObject(with: data!, options: [])
+                print("Response json is: \(jsonRes)")
+            }
+            
+            self.view.backgroundColor = .red
+        }.resume()
     }
 
-
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        let session = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print("There was an error: \(error.localizedDescription)")
+            } else {
+                let jsonRes = try? JSONSerialization.jsonObject(with: data!, options: [])
+                print("The response: \(jsonRes)")
+            }
+        }.resume()
+    }
 }
 
