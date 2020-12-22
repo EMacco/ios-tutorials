a5afc2321576522aea00050717caada265c59daa
Emmanuel Okwara <emma4real37@gmail.com>
2020-12-22 09:39:33 +0100

    URL Session download task

--- DownloadTask/ViewController.swift
+++ DownloadTask/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var downloadImageView: UIImageView!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var progressLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        progressBar.progress = 0
     }
 
-
+    @IBAction func downloadBtnClicked(_ sender: UIButton) {
+        progressLbl.isHidden = false
+        downloadImageView.image = nil
+        let urlString = "https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/rottweiler-card-small.jpg?bust=1535568036"
+        guard let url = URL(string: urlString) else {
+            print("This is an invalid URL")
+            return
+        }
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        session.downloadTask(with: url).resume()
+    }
+    
 }
 
+extension ViewController: URLSessionDownloadDelegate {
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        guard let data = try? Data(contentsOf: location) else {
+            print("The data could not be loaded")
+            return
+        }
+        
+        let image = UIImage(data: data)
+        DispatchQueue.main.async { [weak self] in
+            self?.downloadImageView.image = image
+            self?.progressLbl.isHidden = true
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.progressBar.progress = progress
+            self?.progressLbl.text = "\(progress * 100)%"
+        }
+    }
+}
