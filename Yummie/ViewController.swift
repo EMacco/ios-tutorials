c882463078d50222953fe9d6e46396a7897ddf4b
Emmanuel Okwara <emma4real37@gmail.com>
2021-01-30 04:15:13 +0100

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Emmanuel Okwara on 30/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
