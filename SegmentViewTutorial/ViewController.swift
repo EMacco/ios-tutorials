400f78d0ced4a205538902b49d179c9610c55cdf
Emmanuel Okwara <emma4real37@gmail.com>
2021-09-04 16:47:05 +0100

    Initial Commit

---
+++ SegmentViewTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentViewTutorial
+//
+//  Created by Emmanuel Okwara on 04/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
