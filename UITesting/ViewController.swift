75e59b977b8687fc25e9ab44a031861078157ad7
Emmanuel Okwara <emma4real37@gmail.com>
2021-10-22 09:05:04 +0100

    Initial Commit

---
+++ UITesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITesting
+//
+//  Created by Emmanuel Okwara on 22/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
