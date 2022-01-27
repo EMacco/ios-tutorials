63a97654064094a6803bc42e36206fc70514e4af
Emmanuel Okwara <emma4real37@gmail.com>
2022-01-27 15:44:58 +0100

    basic animations

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alertView: UIView!
+    @IBOutlet weak var alertViewTopConstrain: NSLayoutConstraint!
+    var isActive = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        hideAlert()
     }
 
+    @IBAction func btnClicked(_ sender: Any) {
+//        animateAlert(show: !isActive)
+        springAnimate(show: !isActive)
+    }
+    
+    private func animateAlert(show: Bool) {
+        UIView.animate(withDuration: 0.6) { [weak self] in
+            if show {
+                self?.showAlert()
+            } else {
+                self?.hideAlert()
+            }
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    private func springAnimate(show: Bool) {
+        UIView.animate(withDuration: 1,
+                       delay: 3,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 0.4,
+                       options: []) { [weak self] in
+            if show {
+                self?.showAlert()
+            } else {
+                self?.hideAlert()
+            }
+            self?.view.layoutIfNeeded()
+        } completion: { _ in
+            print("The animation is complete!")
+        }
 
+    }
+    
+    private func showAlert() {
+        isActive = true
+        alertViewTopConstrain.constant = 20
+    }
+    
+    private func hideAlert() {
+        isActive = false
+        alertViewTopConstrain.constant = -(alertView.frame.origin.y) - alertView.frame.height
+    }
 }
 
