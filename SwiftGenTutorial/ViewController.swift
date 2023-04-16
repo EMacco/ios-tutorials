76201d7264b63e09ac364b821caaf740657626b2
Emmanuel Okwara <emma4real37@gmail.com>
2023-04-16 23:56:15 +0200

    Initial Commit

---
+++ SwiftGenTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGenTutorial
+//
+//  Created by Emmanuel Okwara on 16.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
