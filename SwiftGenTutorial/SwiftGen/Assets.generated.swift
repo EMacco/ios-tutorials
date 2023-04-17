14f01f8687988497eebaf635428b09cb57324138
Emmanuel Okwara <emma4real37@gmail.com>
2023-04-17 17:18:38 +0200

    swiftgen finished

---
+++ SwiftGenTutorial/SwiftGen/Assets.generated.swift
@@ -0,0 +1,17 @@
+// 🤖 This file was code-generated. Do not edit!!!
+
+import UIKit
+
+// MARK: - Images
+public extension UIImage {
+  static let facebook: UIImage = UIImage(named: "facebook")!
+  static let google: UIImage = UIImage(named: "google")!
+  static let twitter: UIImage = UIImage(named: "twitter")!
+}
+
+// MARK: - Colors
+public extension UIColor {
+  static let accentColor: UIColor = UIColor(named: "AccentColor")!
+  static let customColor: UIColor = UIColor(named: "custom-color")!
+  static let `default`: UIColor = UIColor(named: "default")!
+}
--- SwiftGenTutorial/ViewController.swift
+++ SwiftGenTutorial/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = .twitter
+        view.backgroundColor = .default
     }
-
-
 }
-
